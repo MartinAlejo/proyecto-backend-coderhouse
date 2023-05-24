43606f8a6341e62536bff1e60f589806024218ad
Martin Polese <mpolese@fi.uba.ar>
2023-05-24 02:47:55 -0300

    Ahora el endpoint pasa de ser /products a ser /api/products

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/products", routerProducts);
+app.use("/api/products", routerProducts);
 
 app.listen(8080, () => console.log("Servidor levantado"))
