b377dd81d4bc01a05a04a341e0eb88e1ceeab101
Martin Polese <mpolese@fi.uba.ar>
2023-06-25 17:27:24 -0300

    Empiezo a trabajar en la vista de products (en principio ya esta todo hecho menos la funcionalidad del botón para agregar al carrito)

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -22,4 +22,17 @@ router.get('/chat', async (req, res) => {
   res.render('chat')
 })
 
+router.get('/products', async (req, res) => {
+  let page = req.query.page
+
+  let products = await productManager.getProducts(2, page, undefined);
+  products.prevLink = products.hasPrevPage?`http://localhost:8080/products?page=${products.prevPage}`:'';
+  products.nextLink = products.hasNextPage?`http://localhost:8080/products?page=${products.nextPage}`:'';
+  
+  res.render('products', {
+    title: "Products",
+    products: products
+  })
+})
+
 export default router;
